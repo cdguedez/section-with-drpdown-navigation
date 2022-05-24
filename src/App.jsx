50acf8f6fac32d2428531fb0fa4572d9fcda97a7
cdguedez <cdgr.ing@gmail.com>
2022-05-23 20:32:14 -0400

    [FEATURE] change background App.jsx on #F1F1F1

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import Header from './components/Header';
 import Home from './pages/Home'
+import styled from 'styled-components'
 
 function App() {
   return (
-    <>
+    <Container>
       <Header/>
       <Home />
-    </>
+    </Container>
   );
 }
 
 export default App;
+
+const Container = styled.div`
+  background-color: #F1F1F1;
+`
