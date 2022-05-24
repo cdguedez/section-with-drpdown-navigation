bed16289a4ac0ecfc186c55b8d6d9610de74af1c
cdguedez <cdgr.ing@gmail.com>
2022-05-23 20:33:07 -0400

    [FEATURE] modify position icons in submenu

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
+import styled from 'styled-components'
 
-const Link = ({ to, icon = null, children, onClick }) => {
+const Link = ({ to, icon = null, children, onClick, isSubmenu = false }) => {
   return (
-    <a
+    <A
       onClick={onClick}
       href={to}
-    >{ children } <img src={icon} alt="" /></a>
+    >
+      {
+        isSubmenu
+          ? <span><img src={icon} alt="" /> { children }</span>
+          : <span>{ children } <img src={icon} alt="" /></span>
+      }
+    </A>
   )
 }
 
 export default Link
+
+
+const A = styled.a`
+  cursor: pointer;
+  text-decoration: none;
+  color: #999FA0;
+`
